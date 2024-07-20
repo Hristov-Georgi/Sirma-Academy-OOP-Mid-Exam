894e335e2a08ac658d34a4c9d025c33b4937686e
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-20 12:29:26 +0300

    changed department data type from String to Department enum

--- src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/entity/Employee.java
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.sirmaacademy.oopexam.employeemanagementsystem.entity;
 
+import com.sirmaacademy.oopexam.employeemanagementsystem.enums.Department;
 import com.sirmaacademy.oopexam.employeemanagementsystem.enums.Role;
 
 import java.math.BigDecimal;
@@ -8,11 +9,9 @@ public class Employee {
 
     private int id;
     private String name;          // TODO: add full name ??
-    private String department;
+    private Department department;
     private Role role;
     private BigDecimal salary;
-
-
 
 
 }
