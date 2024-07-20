658b7e9df47ede130a8f942083d384e968c0d496
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-20 13:06:08 +0300

    implemented Department enumearation

--- src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/enums/Department.java
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/enums/Department.java
@@ -1,5 +1,22 @@
 package com.sirmaacademy.oopexam.employeemanagementsystem.enums;
 
-public class Department {
+
+public enum Department {
+
+    IT("IT"),
+    MARKETING("Marketing"),
+    HUMAN_RESOURCES("Human Resources"),
+    ACCOUNTING("Accounting"),
+    SECURITY("Security");
+
+    private String value;
+
+    private Department(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 
 }
