64d19d370a735e394220630e6580e92a5d98619c
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-20 10:40:25 +0300

    initialize employee management application

---
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/EmployeeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.sirmaacademy.oopexam.employeemanagementsystem;
+
+public class EmployeeManagementApplication {
+    public static void main(String[] args) {
+
+        //Add Employee: Input employee details (ID, name, department, role, salary) and
+        //add them to the system.
+        // Edit Employee: Modify existing employee details.
+        // Fire Employee: Flag that the employee doesn’t work at the company anymore.
+        // List Employees: Display a list of all active employees with their details.
+
+        //TODO: read from console
+    }
+}
