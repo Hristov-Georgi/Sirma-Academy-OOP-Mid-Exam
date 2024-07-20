87d9b8ca00e1fa9d772096cdac773f71d1d2788c
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-20 15:16:59 +0300

    created EmployeeServiceLogic class return single instance

--- src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/service/employeeserviceimpl/EmployeeServiceLogic.java
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/service/employeeserviceimpl/EmployeeServiceLogic.java
@@ -1,16 +1,28 @@
 package com.sirmaacademy.oopexam.employeemanagementsystem.service.employeeserviceimpl;
 
 import com.sirmaacademy.oopexam.employeemanagementsystem.entity.Employee;
+import com.sirmaacademy.oopexam.employeemanagementsystem.repository.EmployeeRepository;
 import com.sirmaacademy.oopexam.employeemanagementsystem.service.EmployeeService;
 
 import java.util.List;
 
 public class EmployeeServiceLogic implements EmployeeService {
+    private static final EmployeeServiceLogic OBJECT_INSTANCE = new EmployeeServiceLogic();
 
+    private final EmployeeRepository employeeRepository;
+
+    private EmployeeServiceLogic() {
+        this.employeeRepository = EmployeeRepository.getInstance();
+    }
+
+    public static EmployeeServiceLogic getInstance() {
+        return OBJECT_INSTANCE;
+    }
 
     @Override
     public List<Employee> findAllByName(String name) {
-        return List.of();
+
+        return this.employeeRepository.findAllByName(name);
     }
 
     @Override
@@ -21,5 +33,35 @@ public class EmployeeServiceLogic implements EmployeeService {
     @Override
     public List<Employee> findAllByDepartment(String department) {
         return List.of();
+    }
+
+    @Override
+    public List<Employee> getAllActive() {
+        return List.of();
+    }
+
+    @Override
+    public void add(Employee employee) {
+
+    }
+
+    @Override
+    public void edit() {
+
+    }
+
+    @Override
+    public void fire() {
+
+    }
+
+    @Override
+    public void saveAllActive() {
+
+    }
+
+    @Override
+    public void saveAllFired() {
+
     }
 }
