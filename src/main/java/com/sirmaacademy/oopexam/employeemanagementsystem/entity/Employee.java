ac0b0d9d078ffd64b3925c62fd257b71b7cf4735
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-20 12:24:34 +0300

    changed id data type

--- src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/entity/Employee.java
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/entity/Employee.java
@@ -1,5 +1,18 @@
 package com.sirmaacademy.oopexam.employeemanagementsystem.entity;
 
+import com.sirmaacademy.oopexam.employeemanagementsystem.enums.Role;
+
+import java.math.BigDecimal;
+
 public class Employee {
+
+    private int id;
+    private String name;          // TODO: add full name ??
+    private String department;
+    private Role role;
+    private BigDecimal salary;
+
+
+
 
 }
