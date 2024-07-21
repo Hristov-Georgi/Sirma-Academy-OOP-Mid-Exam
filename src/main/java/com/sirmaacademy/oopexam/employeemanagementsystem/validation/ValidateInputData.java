9e313422a5e2d279caddd5c91c2960149efcd43e
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-21 17:43:59 +0300

    fixed input validation methods

--- src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/validation/ValidateInputData.java
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/validation/ValidateInputData.java
@@ -9,7 +9,6 @@ import java.io.InvalidObjectException;
 public abstract class ValidateInputData {
     private static final double MINIMAL_SALARY = 933;
 
-    //fixed not tested
     public static double validateSalary(String salary) throws NumberFormatException {
 
         double remuneration = Double.parseDouble(salary);
@@ -19,7 +18,6 @@ public abstract class ValidateInputData {
         return remuneration;
     }
 
-    //fixed not checked
     public static Role validateRole(String role) throws IOException {
         Role[] values = Role.values();
 
@@ -33,7 +31,6 @@ public abstract class ValidateInputData {
         throw new IllegalArgumentException("Role \"" + role + "\" does not exist.");
     }
 
-    //fixed not checked
     public static Department validateDepartment(String department) throws IOException {
 
         Department[] values = Department.values();
@@ -48,17 +45,16 @@ public abstract class ValidateInputData {
         throw new IllegalArgumentException("Department \"" + department + "\" does not exist.");
     }
 
-    //fixed not checked
     public static String validateName(String name) throws IOException {
 
         if (name.isBlank() || name.length() < 2) {
-            throw new InvalidObjectException("Name should be 2 or more symbols long. Enter valid name:");
+            throw new InvalidObjectException("Name should be 2 or more symbols long.");
         }
 
         for (char s : name.toCharArray()) {
 
             if (s < 65 || 90 < s && s < 97 || 122 < s) {
-                throw new IllegalArgumentException("Name should contain only letters. Enter valid name:");
+                throw new IllegalArgumentException("Name should contain only letters.");
             }
 
         }
