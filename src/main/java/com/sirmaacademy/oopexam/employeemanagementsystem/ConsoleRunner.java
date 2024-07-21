5a271a9633397b99df1bcb3993d92cc76c3f31fe
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-21 14:32:04 +0300

    implemented get and print all active employees function

--- src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/ConsoleRunner.java
+++ src/main/java/com/sirmaacademy/oopexam/employeemanagementsystem/ConsoleRunner.java
@@ -36,7 +36,7 @@ public abstract class ConsoleRunner {
         System.out.println("6 - Search by first name");
         System.out.println("7 - Search by last name");
         System.out.println("8 - Search by first and last names");
-        System.out.println("9 - Display all active employees (non discharged)");
+        System.out.println("9 - Display all active employees (not fired (discharged))");
         System.out.println("10 - Save all manipulated data and exit from Employee Management System");
         System.out.println();
     }
@@ -177,7 +177,8 @@ public abstract class ConsoleRunner {
                         break;
 
                     case 9:
-
+                        List<Employee> activeEmployees = EMPLOYEE_SERVICE.getAllActive();
+                        printListData(activeEmployees);
                         break;
 
                     case 10:
